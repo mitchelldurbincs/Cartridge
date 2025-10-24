a9446baf9adc504d1a00a5d0839ece3ced51eef3
mitchelldurbincs <68557654+mitchelldurbincs@users.noreply.github.com>
2025-10-24 09:42:30 -0400

    perf(actor): reduce locking overhead

--- services/actor-rust/build.rs
+++ services/actor-rust/build.rs
@@ -1,13 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Generate protobuf code for engine and replay services
-    tonic_build::configure()
-        .build_server(true)
-        .compile(
-            &[
-                "../../proto/engine/v1/engine.proto",
-                "../../proto/replay/v1/replay.proto",
-            ],
-            &["../../proto"],
-        )?;
+    tonic_build::configure().build_server(true).compile(
+        &[
+            "../../proto/engine/v1/engine.proto",
+            "../../proto/replay/v1/replay.proto",
+        ],
+        &["../../proto"],
+    )?;
     Ok(())
 }
