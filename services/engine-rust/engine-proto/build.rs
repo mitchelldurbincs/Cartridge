c3ac1f95b44495ea1273179b52c30c5de45aa8b8
mitchelldurbincs <68557654+mitchelldurbincs@users.noreply.github.com>
2025-10-25 09:25:18 -0400

    fix(engine): resolve proto path in build script

--- services/engine-rust/engine-proto/build.rs
+++ services/engine-rust/engine-proto/build.rs
@@ -1,14 +1,17 @@
+use std::{env, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_file = "../../../proto/engine/v1/engine.proto";
-    let proto_dir = "../../../proto";
-    
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+    let proto_dir = manifest_dir.join("../../../proto");
+    let proto_file = proto_dir.join("engine/v1/engine.proto");
+
     // Tell cargo to invalidate the built crate whenever the proto file changes
-    println!("cargo:rerun-if-changed={}", proto_file);
-    
+    println!("cargo:rerun-if-changed={}", proto_file.display());
+
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .compile(&[proto_file], &[proto_dir])?;
-    
+
     Ok(())
 }
